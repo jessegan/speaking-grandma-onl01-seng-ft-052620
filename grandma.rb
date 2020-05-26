5f738caa1d9c94ccc88f61a18ea2336fb27f5899
Jesse Gan <jesse13579@gmail.com>
2020-05-26 18:03:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,9 @@ def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN"
   elsif input != input.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
+  end 
 
 end
